75de4f454b9e641e087fbf5e8b88670cf89af97c
Jeffrey Karikari <jeffreyk31k@gmail.com>
2024-04-26 02:33:33 +0000

    task 4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,39 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    
+      this._amount = value;
+    
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+  
+      this._currency = value;
+    
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount === 'number' && typeof conversionRate === 'number') {
+      return amount * conversionRate;
+    }
+    throw new TypeError('Amount and conversionRate must be numbers.');
+  }
+}
